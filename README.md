eb796cb6c215ee25e16058da7c904f667b2a22ed
nn8ninsyssky <nn8ninsyssky@gmail.com>
2026-06-16 18:25:00 +0530

    requirements changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # medibot
+
+### Steps
+
+#git colne "https://github.com/nn8ninsyssky/medibot.git"
+
+# Step 01
+#conda create -n medibot python=3.12.13 -y
+
+#conda activate medibot
+
+#pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medibot",
+    version="0.1.0",
+    author="Diptanu",
+    author_email="nn8ninsyssky@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
